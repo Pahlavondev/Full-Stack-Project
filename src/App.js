95e80a7a84357f2f1c1114c4f3c0067349888f84
Qayumov Pahlavon <qayumovpahlavon2778tatu@gmail.com>
2023-01-17 18:42:38 +0500

    navbar components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { Main, Login, Register } from "./components";
+import { Main, Login, Register, Navbar } from "./components";
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
