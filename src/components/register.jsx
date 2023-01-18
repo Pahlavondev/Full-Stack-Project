6cf3eefd1ab6fc3a36d55e5f868a0420312a8f45
Qayumov Pahlavon <qayumovpahlavon2778tatu@gmail.com>
2023-01-18 10:02:23 +0500

    login and register components added

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { icon } from "../constants/index";
+import { Input } from "../ui";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="container text-center mt-5">
+      <main className="form-signin w-25 m-auto">
+        <form>
+          <img className="mb-2" src={icon} alt="" width="72" height="60" />
+          <h1 className="h3 mb-3 fw-normal">Please register</h1>
+
+          <Input label={"Username"} state={name} setState={setName} />
+
+          <Input
+            label={"Email Address"}
+            type={"email"}
+            state={email}
+            setState={setEmail}
+          />
+
+          <Input
+            label={"Password"}
+            type={"password"}
+            state={password}
+            setState={setPassword}
+          />
+
+          <button className="w-100 btn btn-lg btn-primary" type="submit">
+            Register
+          </button>
+        </form>
+      </main>
+    </div>
+  );
 };
 
 export default Register;
