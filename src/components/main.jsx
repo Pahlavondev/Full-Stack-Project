e20542a1ded8867a1255597b9a845747dee725b6
Qayumov Pahlavon <qayumovpahlavon2778tatu@gmail.com>
2023-01-26 21:48:26 +0500

    Add loader components

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Loader } from "../ui";
 
 const Main = () => {
-  const { articles } = useSelector((state) => state.article);
+  const { articles, isLoading } = useSelector((state) => state.article);
 
   return (
     <div className="container">
+      {isLoading && <Loader />}
       <div className="album py-5">
         <div className="container">
           <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
@@ -24,36 +26,38 @@ const Main = () => {
                   >
                     <rect width="100%" height="100%" fill="#55595c"></rect>
                   </svg>
+
                   <div className="card-body">
-                    <p className="card-text fw-bold">{item.title}</p>
+                    <p className="card-text fw-bold m-0">{item.title}</p>
                     <p className="card-text">{item.description}</p>
-                    <div className="d-flex justify-content-between align-items-center">
-                      <div className="btn-group">
-                        <button
-                          type="button"
-                          className="btn btn-sm btn-outline-success"
-                        >
-                          View
-                        </button>
+                  </div>
 
-                        <button
-                          type="button"
-                          className="btn btn-sm btn-outline-secondary"
-                        >
-                          Edit
-                        </button>
+                  <div className="card-footer d-flex justify-content-between align-items-center">
+                    <div className="btn-group">
+                      <button
+                        type="button"
+                        className="btn btn-sm btn-outline-success"
+                      >
+                        View
+                      </button>
 
-                        <button
-                          type="button"
-                          className="btn btn-sm btn-outline-danger"
-                        >
-                          Delete
-                        </button>
-                      </div>
-                      <small className="text-muted fw-bold text-capitalize">
-                        {item.author.username}
-                      </small>
+                      <button
+                        type="button"
+                        className="btn btn-sm btn-outline-secondary"
+                      >
+                        Edit
+                      </button>
+
+                      <button
+                        type="button"
+                        className="btn btn-sm btn-outline-danger"
+                      >
+                        Delete
+                      </button>
                     </div>
+                    <small className="text-muted fw-bold text-capitalize">
+                      {item.author.username}
+                    </small>
                   </div>
                 </div>
               </div>
